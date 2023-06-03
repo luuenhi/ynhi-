79fdad55eb43c005d18575ba021b5ff2e1d933db
Luu Yen Nhi <luuenhi03@gmail.com>
2023-06-03 23:07:38 +0700

    aims project include aims class, digitalVideoDiscClass, and lab03

---
+++ AimsProject/src/DVD.java
@@ -0,0 +1,13 @@
+
+public class DVD {
+
+	public DVD(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public char[] getTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ AimsProject/src/hust/soict/dsai/test/cart/CartTest.java
@@ -0,0 +1,34 @@
+package hust.soict.dsai.test.cart;
+
+public class CartTest {
+	public static void main(String[] args) {
+		 CartTest cart = new CartTest();
+		 
+		 DigitalVideoDisc dvd1 = new DigitalVideoDisc();
+		 cart.addDigitalVideoDisc(dvd1);
+		 
+		 
+		 DigitalVideoDisc dvd2 = new DigitalVideoDisc();
+		 cart.addDigitalVideoDisc(dvd2);
+		 
+		 DigitalVideoDisc dvd3 = new DigitalVideoDisc();
+		 cart.addDigitalVideoDisc(dvd3);
+		 
+		 cart.print();
+		 
+		 
+		 
+
+	}
+
+	private void print() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void addDigitalVideoDisc(DigitalVideoDisc dvd1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

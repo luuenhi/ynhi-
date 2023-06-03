0c68c5b7441a73373e18adf2cba73bbed801e354
Luu Yen Nhi <luuenhi03@gmail.com>
2023-06-03 23:07:38 +0700

    aims project

---
+++ AimsProject/src/hust/soict/dsai/store/DVD.java
@@ -0,0 +1,14 @@
+package hust.soict.dsai.store;
+
+public class DVD {
+
+	public DVD(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public char[] getTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
